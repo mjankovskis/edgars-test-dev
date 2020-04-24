c5c1aad25b374abb7735bcbe9bbccf5b08f64837
Miķelis Jankovskis <mikelis.jankovskis@mapon.com>
2020-04-24 10:50:43 +0300

    Added my js.

--- src/my.js
+++ src/my.js
@@ -2,5 +2,12 @@
 
 import "./css/my.scss";
 
-// Say hello
-console.log("🦊 Hello! Edit me in src/index.js");
+
+$(function() {
+  $('.content-column-content blockquote').addClass('blockquote');
+  $('.content-column-content table').addClass('table');
+  $('.content-column-content table').wrap('<div class="table-responsive" />');
+  $('.content-column-content table thead').addClass('thead-dark');
+  $('.content-column-content pre').wrap('<figure class="highlight" />');
+  $('.content-column-content figure > img').addClass('img-fluid');
+});
